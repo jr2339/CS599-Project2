ebbd7acfa0835c1e0c73db4cf6226912954c56aa
jun <jr2339@nau.edu>
2016-09-22 13:33:19 -0700

    Init

---
+++ Raycast/Raycast/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Raycast
+//
+//  Created by jr2339 on 9/22/16.
+//  Copyright © 2016 jr2339. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
